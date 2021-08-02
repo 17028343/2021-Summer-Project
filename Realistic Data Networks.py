57cdfe7abd6e8f60afa854075fa7a3676d62b5d5
17028343 <85179779+17028343@users.noreply.github.com>
2021-08-02 15:18:59 +0100

    Add files via upload

--- Realistic Data Networks.py
+++ Realistic Data Networks.py
@@ -36,7 +36,7 @@ Poisson_Noise_C=np.random.poisson(lam=Noiseless_C)
 Poisson_Noise=np.c_[Poisson_Noise_A, Poisson_Noise_B, Poisson_Noise_C]
 
 np.random.seed(6)
-Gaussian_Noise=np.random.normal(0, 140, size=Training_Set_Size)
+Gaussian_Noise=np.random.normal(0, 140, size=(Training_Set_Size, 3))
 
 # These are the Noiseless Y values
 x_train = np.transpose(Peak_Height*np.exp(-(X_Values[np.newaxis,].T-Shift)**2/(2*Standard_Deviation**2)))
@@ -44,11 +44,24 @@ x_train = np.transpose(Peak_Height*np.exp(-(X_Values[np.newaxis,].T-Shift)**2/(2
 # Adding the noise to the generated Y values
 for i in np.arange(0,3,1):
     for j in np.arange(0,Training_Set_Size,1):
-        x_train[j,i]=+np.sqrt(Poisson_Noise[j,i]**2+Gaussian_Noise[j]**2)
+        x_train[j,i]=+np.sqrt(Poisson_Noise[j,i]**2+Gaussian_Noise[j,i]**2)
         
+
+x_train_scaled=np.array(x_train, dtype="float32")
+
+for i in np.arange(0,90000,1):
+    x_train_scaled[i,0]=x_train[i,0]/np.max(x_train, axis=1)[i]
+    x_train_scaled[i,1]=x_train[i,1]/np.max(x_train, axis=1)[i]
+    x_train_scaled[i,2]=x_train[i,2]/np.max(x_train, axis=1)[i]
 
 # The true values of each distribution's shift, height and width - Y_Train is the training data targets
 y_train=np.c_[Shift, Peak_Height, Standard_Deviation]
+
+
+y_train_scaled=np.array(y_train, dtype="float32")
+
+for i in np.arange(0,90000,1):
+    y_train_scaled[i,1]=y_train[i,1]/np.max(x_train, axis=1)[i]
 
 
 # allocating the size of the testing data set
@@ -77,7 +90,7 @@ Poisson_Noise_C_Test=np.random.poisson(lam=Noiseless_C_Test)
 Poisson_Noise_Test=np.c_[Poisson_Noise_A_Test, Poisson_Noise_B_Test, Poisson_Noise_C_Test]
 
 np.random.seed(13)
-Gaussian_Noise_Test=np.random.normal(0, 140, size=Test_Set_Size)
+Gaussian_Noise_Test=np.random.normal(0, 140, size=(Test_Set_Size, 3))
 
 # These are the Noiseless Y values
 x_test = np.transpose(Peak_Height_Test*np.exp(-(X_Values[np.newaxis,].T-Shift_Test)**2/(2*Standard_Deviation_Test**2)))
@@ -85,11 +98,24 @@ x_test = np.transpose(Peak_Height_Test*np.exp(-(X_Values[np.newaxis,].T-Shift_Te
 # Adding the noise to the generated Y values
 for i in np.arange(0,3,1):
     for j in np.arange(0,Test_Set_Size,1):
-        x_test[j,i]=+np.sqrt(Poisson_Noise_Test[j,i]**2+Gaussian_Noise_Test[j]**2)
+        x_test[j,i]=+np.sqrt(Poisson_Noise_Test[j,i]**2+Gaussian_Noise_Test[j,i]**2)
+        
+
+x_test_scaled=np.array(x_test, dtype="float32")
+
+for i in np.arange(0,10000,1):
+    x_test_scaled[i,0]=x_test[i,0]/np.max(x_test, axis=1)[i]
+    x_test_scaled[i,1]=x_test[i,1]/np.max(x_test, axis=1)[i]
+    x_test_scaled[i,2]=x_test[i,2]/np.max(x_test, axis=1)[i]
         
 
 # The true values of each distribution's shift, height and width - Y_test is the testing data targets
 y_test=np.c_[Shift_Test, Peak_Height_Test, Standard_Deviation_Test]
+
+y_test_scaled=np.array(y_test, dtype="float32")
+
+for i in np.arange(0,10000,1):
+    y_test_scaled[i,1]=y_test[i,1]/np.max(x_test, axis=1)[i]
 
 
 # Showing the Train and Test Gaussians
@@ -135,15 +161,15 @@ def get_model(mc=False, act="relu"):
 
 mc_model = get_model(mc=True, act="selu")
 
-#earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 batch_size=120
 
-h_mc = mc_model.fit(x_train, y_train,
+h_mc = mc_model.fit(x_train_scaled, y_train_scaled,
               batch_size=batch_size,
               epochs=200,
               verbose=1,
-              validation_data=(x_test, y_test),
-#              callbacks=earlystop,
+              validation_data=(x_test_scaled, y_test_scaled),
+              callbacks=earlystop,
                     )
 
 # Plotting the Accuracy of the Model over time, both training and validation data
@@ -171,11 +197,15 @@ import tqdm
 # Monte Carlo Network Predictions
 mc_predictions = []
 for i in tqdm.tqdm(range(500)):
-    y_p = mc_model.predict(x_test, batch_size=1000)
+    y_p = mc_model.predict(x_test_scaled, batch_size=1000)
     mc_predictions.append(y_p)
 
 mc_predictions=np.array(mc_predictions, dtype="float32")
 montcarlpred=np.median(mc_predictions, axis=0)
+
+
+for i in np.arange(0,10000,1):
+    montcarlpred[i,1]=montcarlpred[i,1]*np.max(x_test, axis=1)[i]
 
 # Implementing the Analytical solutions to the gaussians
 a = np.log(x_test[:,2]/x_test[:,1])
@@ -194,8 +224,8 @@ Analytic_Parameters = np.c_[Analytic_Shift, Analytic_Height, Analytic_Width]
 
 
 # Showing the True, Network and Analytic Gaussians
-Liner=np.linspace(1074.4,1074.8,1000)
-GaussianNumber= 88 # within [0,10000]
+Liner=np.linspace(1000,1105,1000)
+GaussianNumber= 1000 # within [0,10000]
 plt.plot(Liner, 
             montcarlpred[GaussianNumber,1] * np.exp(-(Liner-montcarlpred[GaussianNumber,0])*(Liner-montcarlpred[GaussianNumber,0])/(2*montcarlpred[GaussianNumber,2]*montcarlpred[GaussianNumber,2])),
             c='b', zorder=1)
@@ -213,8 +243,6 @@ plt.show()
 print(montcarlpred[GaussianNumber,0], Analytic_Parameters[GaussianNumber,0], y_test[GaussianNumber,0])
 print(montcarlpred[GaussianNumber,1], Analytic_Parameters[GaussianNumber,1], y_test[GaussianNumber,1])
 print(montcarlpred[GaussianNumber,2], Analytic_Parameters[GaussianNumber,2], y_test[GaussianNumber,2])
-
-
 
 
 
